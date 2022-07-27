3b92782796a1bd3bab28ad6650c6943a14be35c1
imtiaz <imtiazjavatrainer@gmail.com>
2022-07-27 17:00:40 +0530

    fc

---
+++ src/main/java/com/cgi/main/EmsappApplication.java
@@ -0,0 +1,13 @@
+package com.cgi.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsappApplication.class, args);
+	}
+
+}
